a040393c259ffede1ff26e2aacf4ca57dd32d07a
Kouassi Henri Joel <khenrijo21@gmail.com>
2024-03-13 21:11:45 +0000

    prettier config added

--- prettier.js
+++ prettier.js
@@ -1,7 +1,8 @@
 module.exports = {
-  "singleQuote": true,
-  "tabWidth": 2,
-  "printWidth": 120,
-  "trailingComma": "es5",
-  "plugins": ["prettier-plugin-tailwindcss"]
-} 
+  parser: 'babel',
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
